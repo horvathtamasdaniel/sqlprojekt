6176488278d5c6a7a9f27634e975158009318d90
Horváth Bátor <batorko@gmail.com>
2018-05-23 15:03:45 +0200

    vissza

--- client.js
+++ client.js
@@ -16,28 +16,32 @@ function sql(sqlText) {
 }
 
 //Ebben a függvényben dolgozd fel a kapott objektumot: jelenítsd meg az adatokat az oldalon
-function refreshDocument(jsonObject){
-	console.log(jsonObject);
+function refreshDocument(jsonObject) {
+    console.log(jsonObject);
     document.getElementById('content').innerText = JSON.stringify(jsonObject);
 }
 
-function kekFules(){
-    var sqlText = "SELECT colour, price, handle, prsize, stock FROM products WHERE colour = 'kék' AND handle <> 0"; 
+function kekFules() {
+    var sqlText = "SELECT colour, price, handle, prsize, stock FROM products WHERE colour = 'kék' AND handle <> 0";
     sql(sqlText);
 }
-function nincsKeszlet(){
-    var sqlText = "SELECT * FROM products WHERE stock=0"; 
+
+function nincsKeszlet() {
+    var sqlText = "SELECT * FROM products WHERE stock=0";
     sql(sqlText);
 }
-function tobbDarab(){
-    var sqlText = "SELECT Customer.name, COUNT(*) AS darab FROM orders JOIN customer ON customer.id = orders.customerid GROUP BY orders.customerid HAVING COUNT(*) > 1"; 
+
+function tobbDarab() {
+    var sqlText = "SELECT Customer.name, COUNT(*) AS darab FROM orders JOIN customer ON customer.id = orders.customerid GROUP BY orders.customerid HAVING COUNT(*) > 1";
     sql(sqlText);
 }
-function tobbRendelesNap(){
-    var sqlText = "SELECT orders.date, COUNT(*) AS darab FROM orders JOIN customer ON customer.id = orders.customerid GROUP BY orders.date HAVING COUNT(*) > 1"; 
+
+function tobbRendelesNap() {
+    var sqlText = "SELECT orders.date, COUNT(*) AS darab FROM orders JOIN customer ON customer.id = orders.customerid GROUP BY orders.date HAVING COUNT(*) > 1";
     sql(sqlText);
 }
-function balatonRendeles(){
-    var sqlText = "SELECT DISTINCT customer.name, customer.address FROM orders JOIN customer ON customer.id = orders.customerid WHERE customer.address LIKE '8%'"; 
+
+function balatonRendeles() {
+    var sqlText = "SELECT DISTINCT customer.name, customer.address FROM orders JOIN customer ON customer.id = orders.customerid WHERE customer.address LIKE '8%'";
     sql(sqlText);
 }
