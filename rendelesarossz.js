32906ce6d7cf546aeb70dc54e9212b463a887738
Horváth Bátor <batorko@gmail.com>
2018-05-23 15:15:04 +0200

    keszkod

--- rendelesarossz.js
+++ rendelesarossz.js
@@ -1,4 +1,4 @@
 function rendelesarossz() {
-    var sqlText = "SELECT DISTINCT customer.name, customer.address FROM orders JOIN customer ON customer.id = orders.customerid WHERE customer.address LIKE '8%'";
+    var sqlText = "SELECT SUM(products.price) FROM orders JOIN products ON orders.productid=products.prid";
     sql(sqlText);
 }
